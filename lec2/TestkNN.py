5f839f06cdddfca5ff3e5263857dd69dbd0c05f2
tantexian <tantexian@qq.com>
2017-07-11 00:14:31 +0800

    增加注释

--- lec2/TestkNN.py
+++ lec2/TestkNN.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+from numpy import tile
+
 import kNN
 import unittest
 
@@ -10,9 +13,14 @@ class TestkNN(unittest.TestCase):
 
     def test_classify0(self):
         group, labels = kNN.createDateSet()
+        # 根据输入的【0，0】判别出属于哪种类型（labels）
         result = kNN.classifyO([0, 0], group, labels, 3)
         print("result == %s" % (result))
 
+    def test_tile(self):
+        intX = [1,2]
+        x = tile(intX, 2)
+        print("\ntile(intX) == %s" %(x))
 
 if __name__ == '__main__':
     unittest.main()
--- lec2/kNN.py
+++ lec2/kNN.py
@@ -12,20 +12,26 @@ def createDateSet():
     return group, labels
 
 
-# intX 分类的输人向量
-# dataSet 输人的训练样本集
-# labels 标签向量
-# k 表示用于选择最近邻居的数目
+# intX 分类的输人向量 【0，0】
+# dataSet 输人的训练样本集（上述group）
+# labels 标签向量 （labels）
+# k 表示用于选择最近邻居的数目 （k==3）
 def classifyO(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
+    dataSetSize = dataSet.shape[0]  # 得到数组的行数
+    # 变成和dataSet一样的行数,行数=原来*numSamples，列数=原来*1 ，然后每个特征点和样本的点进行相减
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
+    # axis=1为按行求和(0 为按列求和)
     sqDistances = sqDiffMat.sum(axis=1)
+    # 平方根
     distances = sqDistances ** 0.5
+    # 排序
     sortedDistIndicies = distances.argsort()
     classCount = {}
+    # 取排序后前k个值
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    # 返回前K个点出现频率最高的类别作为当前点的预测分类
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
